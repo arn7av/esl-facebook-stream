f588fc0a9fc4f62cd8ce7b3a2072995386a1dee9
Arnav <arnav@arnav.at>
2018-05-20 17:02:32 +0530

    [Minor][BE]support for upcoming ESL One events

--- src/esl_facebook_server/esl_events_config.py
+++ src/esl_facebook_server/esl_events_config.py
@@ -5,10 +5,21 @@ ESL_EVENT_FAMILY_DICT = OrderedDict([
         'one',
         {
             'event_domain': 'live.esl-one.com',
-            'event_path': '/',
-            'event_id_list': [11093],
+            # 'event_path': '/',
+            'event_path': '/birmingham',
+            'event_id_list': [11092],
             'event_facebook_list': ['WatchESLOne'],
             'weight': 1,
+        }
+    ),
+    (
+        'one_alt',
+        {
+            'event_domain': 'live.esl-one.com',
+            'event_path': '/cologne',
+            'event_id_list': [11224],
+            'event_facebook_list': ['WatchESLOne'],
+            'weight': 2,
         }
     ),
     (
@@ -18,7 +29,7 @@ ESL_EVENT_FAMILY_DICT = OrderedDict([
             'event_path': '/csgo',
             'event_id_list': [4090],
             'event_facebook_list': ['ESLProLeagueCSGO'],
-            'weight': 2,
+            'weight': 3,
         }
     ),
     (
@@ -28,7 +39,7 @@ ESL_EVENT_FAMILY_DICT = OrderedDict([
             'event_path': '/',
             'event_id_list': [11062],
             'event_facebook_list': ['WatchIEM'],
-            'weight': 3,
+            'weight': 4,
         }
     ),
 ])
